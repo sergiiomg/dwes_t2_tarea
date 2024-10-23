0426f9ff44e844a99d7293090d5764e1fcacb5f7
Sergio <sergiiomgdaw@gmail.com>
2024-10-23 08:47:42 +0200

    Creación de la clase padre Miembro con sus atributos y métodos.

---
+++ clases/Miembro.php
@@ -0,0 +1,59 @@
+<?php
+    abstract class Miembro{
+        #Atributos de la clase padre Miembro
+        private $id;
+        private $nombre;
+        private $apellidos;
+        private $email;
+        private $edad;
+
+        #Métodos de la clase padre Miembro
+
+        //CONSTRUCTOR
+        public function __construct($id, $nombre, $apellidos, $email, $edad){
+            $this->id = $id;
+            $this->nombre = $nombre;
+            $this->apellidos = $apellidos;
+            $this->email = $email;
+            $this->edad = $edad;
+        }
+
+        //GETTERS
+        public function getId(){
+            return $this->id;
+        }
+        public function getNom(){
+            return $this->nombre;
+        }
+        public function getApellidos(){
+            return $this->apellidos;
+        }
+        public function getEmail(){
+            return $this->email;
+        }
+        public function getEdad(){
+            return $this->edad;
+        }
+
+        //SETTERS
+        public function setId($id){
+            $this->id = $id;
+        }
+        public function setNombre($nombre) {
+            $this->nombre = $nombre;
+        }
+        public function setApellidos($apellidos) {
+            $this->apellidos = $apellidos;
+        }
+        public function setEmail($email) {
+            $this->email = $email;
+        }
+        public function setEdad($edad) {
+            $this->edad = $edad;
+        }
+
+        //TOSTRING
+        public function tostring(){
+            return $this->id ." ". $this->nombre . " " . $this->apellidos . " " . $this->email . " " . $this->edad;
+        }
+    }
